5de625ac76db0aa9043a997b08d762c1b80c9b46
Paulina Ostolska <pa.ostolska@gmail.com>
2024-06-19 18:09:37 +0200

    category view
    .

--- CulinaryRecipesApp/CulinaryRecipesApp/AssemblyInfo.cs
+++ CulinaryRecipesApp/CulinaryRecipesApp/AssemblyInfo.cs
@@ -1,3 +1,16 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("Montserrat-Thin.ttf")]
+[assembly: ExportFont("Montserrat-ThinItalic.ttf")]
+[assembly: ExportFont("Montserrat-Regular.ttf")]
+[assembly: ExportFont("Montserrat-MediumItalic.ttf")]
+[assembly: ExportFont("Montserrat-Medium.ttf")]
+[assembly: ExportFont("Montserrat-LightItalic.ttf")]
+[assembly: ExportFont("Montserrat-Light.ttf")]
+[assembly: ExportFont("Montserrat-ExtraLightItalic.ttf")]
+[assembly: ExportFont("Montserrat-ExtraLight.ttf")]
+[assembly: ExportFont("Montserrat-Italic.ttf")]
+[assembly: ExportFont("MsMadi-Regular.ttf")]
+[assembly: ExportFont("OoohBaby-Regular.ttf")]
