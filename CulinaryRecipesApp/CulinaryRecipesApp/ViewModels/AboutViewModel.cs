c61b184d16e4e70f618c0bd7284b6e5aab951128
Paulina Ostolska <pa.ostolska@gmail.com>
2024-06-20 11:37:53 +0200

    home, shell etc

--- CulinaryRecipesApp/CulinaryRecipesApp/ViewModels/AboutViewModel.cs
+++ CulinaryRecipesApp/CulinaryRecipesApp/ViewModels/AboutViewModel.cs
@@ -1,18 +1,76 @@
 ﻿using System;
-using System.Windows.Input;
-using Xamarin.Essentials;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace CulinaryRecipesApp.ViewModels
 {
     public class AboutViewModel : BaseViewModel
     {
+        private ObservableCollection<CarouselItemViewModel> _carouselItems;
+        private int _currentIndex;
+        private bool _isAutoScrolling;
+        private readonly int _totalItems;
+
         public AboutViewModel()
         {
-            Title = "About";
-            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
+            _currentIndex = 0;
+            _isAutoScrolling = true;
+            _totalItems = 4; // Ustaw ilość elementów w karuzeli
+            CarouselItems = new ObservableCollection<CarouselItemViewModel>
+            {
+                new CarouselItemViewModel { ImageSource = "Assets/car1.jpg" },
+                new CarouselItemViewModel { ImageSource = "Assets/car2.jpg" },
+                new CarouselItemViewModel { ImageSource = "Assets/car3.jpg" },
+                new CarouselItemViewModel { ImageSource = "Assets/car4.jpg" }
+            };
+
+            StartAutoScroll();
         }
 
-        public ICommand OpenWebCommand { get; }
+        public ObservableCollection<CarouselItemViewModel> CarouselItems
+        {
+            get => _carouselItems;
+            set => SetProperty(ref _carouselItems, value);
+        }
+
+        public int CurrentIndex
+        {
+            get => _currentIndex;
+            set
+            {
+                if (_currentIndex != value)
+                {
+                    _currentIndex = value;
+                    OnPropertyChanged(nameof(CurrentIndex)); // Powiadom o zmianie indeksu
+                }
+            }
+        }
+
+        private async void StartAutoScroll()
+        {
+            while (_isAutoScrolling)
+            {
+                await Task.Delay(3000); // Czas oczekiwania na następny przewijany element
+                CurrentIndex = (_currentIndex + 1) % _totalItems; // Przełącz na następny element
+            }
+        }
+
+        public void StopAutoScroll()
+        {
+            _isAutoScrolling = false;
+        }
+
+        public void ResumeAutoScroll()
+        {
+            _isAutoScrolling = true;
+            StartAutoScroll();
+        }
+    }
+
+    public class CarouselItemViewModel
+    {
+        public string ImageSource { get; set; }
     }
 }
